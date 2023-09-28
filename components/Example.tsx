f4183752a87b3518b8cdecc6ab498fa9219fa1a8
shockValue666 <mapedeveloper@protonmail.com>
2023-09-28 16:27:54 -0700

    main

--- components/Example.tsx
+++ components/Example.tsx
@@ -5,26 +5,6 @@ import {
 } from "@/components/ui/avatar"
 
 import {
-    Calculator,
-    Calendar,
-    CreditCard,
-    Settings,
-    Smile,
-    User,
-} from "lucide-react"
-
-import {
-Command,
-CommandEmpty,
-CommandGroup,
-CommandInput,
-CommandItem,
-CommandList,
-CommandSeparator,
-CommandShortcut,
-} from "@/components/ui/command"  
-
-import {
     Card,
     CardContent,
     CardDescription,
@@ -54,44 +34,6 @@ export  default function Example() {
             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
             <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        <Command className="rounded-lg border shadow-md dark">
-            <CommandInput placeholder="Type a command or search..." />
-            <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup heading="Suggestions">
-                <CommandItem>
-                    <Calendar className="mr-2 h-4 w-4" />
-                    <span>Calendar</span>
-                </CommandItem>
-                <CommandItem>
-                    <Smile className="mr-2 h-4 w-4" />
-                    <span>Search Emoji</span>
-                </CommandItem>
-                <CommandItem>
-                    <Calculator className="mr-2 h-4 w-4" />
-                    <span>Calculator</span>
-                </CommandItem>
-                </CommandGroup>
-                <CommandSeparator />
-                <CommandGroup heading="Settings">
-                <CommandItem>
-                    <User className="mr-2 h-4 w-4" />
-                    <span>Profile</span>
-                    <CommandShortcut>⌘P</CommandShortcut>
-                </CommandItem>
-                <CommandItem>
-                    <CreditCard className="mr-2 h-4 w-4" />
-                    <span>Billing</span>
-                    <CommandShortcut>⌘B</CommandShortcut>
-                </CommandItem>
-                <CommandItem>
-                    <Settings className="mr-2 h-4 w-4" />
-                    <span>Settings</span>
-                    <CommandShortcut>⌘S</CommandShortcut>
-                </CommandItem>
-                </CommandGroup>
-            </CommandList>
-            </Command>
             <Card className="w-[350px] dark">
                 <CardHeader>
                     <CardTitle>Create project</CardTitle>
